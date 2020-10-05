a59548acdd2a3937a138e861e4b3632b657d5a3e
Marine Lasbleis <marine.lasbleis@gmail.com>
2020-10-05 22:05:27 +0200

    think about project

---
+++ coauthorship.py
@@ -0,0 +1,12 @@
+## Header
+
+
+## Extract data from Google Scholar
+# Possible libraries: 
+## scholarly
+## gscholar ? https://github.com/venthur/gscholar Maybe too old.
+## From stack exchange: suggestion to build a webscrapper that may work better: https://stackoverflow.com/questions/13200709/extract-google-scholar-results-using-python-or-r
+
+
+
+## Create a graph
